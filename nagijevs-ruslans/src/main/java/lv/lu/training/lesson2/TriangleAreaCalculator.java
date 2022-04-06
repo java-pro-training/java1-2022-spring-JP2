4cec5a70803da3ab706fe3bef4fb35f37e4b7508
Ruslan <ruslans.nagijevs@gmail.com>
2022-04-07 00:45:28 +0300

    Pilnīgi izpildīts 2. lekcijas mājas darbs - taisnstūra perimetrs un taisnstūra trijstūra laukuma kalkulators.

---
+++ nagijevs-ruslans/src/main/java/lv/lu/training/lesson2/TriangleAreaCalculator.java
@@ -0,0 +1,23 @@
+package lv.lu.training.lesson2;
+
+import java.util.Scanner;
+
+public class TriangleAreaCalculator {
+    public static void main(String[] args){
+        int base = 10;
+        int height = 20;
+        int result = (base*height)/2;
+        System.out.println("Trijstūra laukums: " + result);
+
+        //Vēl viens kods - bišiņ interesantāks
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Ievadiet trijstūra pamatu: ");
+        int pamats = scan.nextInt();
+        System.out.print("Ievadiet trijstūra augstumu: ");
+        int augstums = scan.nextInt();
+        int laukums = (pamats * augstums)/2;
+        System.out.println("Trijstūra laukums ir " + laukums);
+
+    }
+}
