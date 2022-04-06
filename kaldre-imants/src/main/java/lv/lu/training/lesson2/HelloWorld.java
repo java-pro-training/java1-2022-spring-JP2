57407fad51102ef035e59d8d970b2ef67cd2280a
Imants Kaldre <imants,kaldre@gmail.com>
2022-04-06 21:58:43 +0300

    Hello world programma

--- kaldre-imants/src/main/java/lv/lu/training/lesson2/HelloWorld.java
+++ kaldre-imants/src/main/java/lv/lu/training/lesson2/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         String part1="Hello";
         String part2="World!";
-        String greeting=part1 +" "+ part2;
+        String greeting=part1 +"     "+ part2;
         System.out.println(greeting);
     }
 
