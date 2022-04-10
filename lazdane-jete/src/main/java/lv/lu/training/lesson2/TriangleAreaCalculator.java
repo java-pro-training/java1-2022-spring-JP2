658f446b84383f0e8c781ba4b2abefd0c863d928
jlazdane <jlazdane@gmail.com>
2022-04-10 18:39:32 +0300

    izveidota trijstura laukuma programma

---
+++ lazdane-jete/src/main/java/lv/lu/training/lesson2/TriangleAreaCalculator.java
@@ -0,0 +1,15 @@
+package lv.lu.training.lesson2;
+
+public class TriangleAreaCalculator {
+    public static void main(String[] args) {
+
+        int base = 27;
+        int height = 42;
+
+        int result = (base * height) / 2;
+
+        System.out.println("the result is: " + result);
+
+    }
+
+}
