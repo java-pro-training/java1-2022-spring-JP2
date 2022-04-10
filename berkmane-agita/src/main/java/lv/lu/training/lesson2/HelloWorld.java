190e46b4e188682fceb1a54654022722ec1fe999
agita <agitaberkmane@gmail.com>
2022-04-10 16:02:52 +0300

    P(četrstūrim), ja dotas visas malas.

--- berkmane-agita/src/main/java/lv/lu/training/lesson2/HelloWorld.java
+++ berkmane-agita/src/main/java/lv/lu/training/lesson2/HelloWorld.java
@@ -5,22 +5,9 @@ import java.sql.SQLOutput;
 public class HelloWorld {
     public static void main (String[] args){
 
-
-
-        String greeting = "Hello World!";
-        System.out.println(greeting);
-
-        System.out.println("Hello World");
-        System.out.println(125);
-        System.out.println('A');
-        System.out.println("***********");
-
-        String part1 = "Hello ";
-        String part2 = "World ";
-        System.out.println(part1 + part2);
-
-        System.out.println("***********");
-        String izdr = part1 + part2;
-        System.out.println(izdr);
-    }
+        String part1 = "Hello";
+        String part2 = "World";
+        String result = part1 + " " + part2;
+        System.out.println(result);
+       }
 }
