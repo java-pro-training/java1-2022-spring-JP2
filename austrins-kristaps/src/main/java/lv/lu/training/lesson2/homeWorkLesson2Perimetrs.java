0c7a9466bb6d59469f5884c3b4ff4355af85e8d8
kristapsAustrins <kristaps.austrins@gmail.com>
2022-04-11 18:59:16 +0300

    MājasdarbsLesson2

--- austrins-kristaps/src/main/java/lv/lu/training/lesson2/homeWorkLesson2Perimetrs.java
+++ austrins-kristaps/src/main/java/lv/lu/training/lesson2/homeWorkLesson2Perimetrs.java
@@ -1,6 +1,6 @@
 package lv.lu.training.lesson2;
 
-public class homeWorkLeasson2Perimetrs {
+public class homeWorkLesson2Perimetrs {
     public static void main(String[] args) {
 
         int side1 = 10;
