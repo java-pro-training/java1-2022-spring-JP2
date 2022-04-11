9b0dc95bf3788ef0078d9bc836920d8d57406d40
andra <andra.paegle@gmail.com>
2022-04-11 21:36:48 +0300

    Izveidota Hello World programma

---
+++ paegle-andra/src/main/java/lv/lu/training/lesson2/homeWorkPerimetrs.java
@@ -0,0 +1,17 @@
+package lv.lu.training.lesson2;
+
+public class homeWorkPerimetrs {
+    public static void main(String[] args) {
+        int sizeA = 10;
+        int sizeB = 15;
+        int sizeC = 11;
+        int sizeD = 20;
+        int result = sizeA;
+        result = result + sizeB;
+        result += sizeC;
+        result += sizeD;
+
+        System.out.println("Četrstūra perimetrs: " + result);
+
+    }
+}
