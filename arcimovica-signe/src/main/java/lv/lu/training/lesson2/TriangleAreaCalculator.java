fca04865cb39c59ca8852b93038983c0fd694908
signe <signe.arcimovica@gmail.com>
2022-04-12 09:22:10 +0300

    Izveidoti 2.lekcijas mājasdarbi.

---
+++ arcimovica-signe/src/main/java/lv/lu/training/lesson2/TriangleAreaCalculator.java
@@ -0,0 +1,13 @@
+package lv.lu.training.lesson2;
+
+public class TriangleAreaCalculator {
+    public static void main (String[] args) {
+        int base = 10;
+        int height = 20;
+        int result1 = base * height;
+        int result = result1 /2; 
+        System.out.println(result);
+
+           }
+    }
+
