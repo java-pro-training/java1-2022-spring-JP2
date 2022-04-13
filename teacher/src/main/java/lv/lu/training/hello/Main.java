0456cd1341780e7d9003c81cda472e7f4a485c4f
Olegs Ganzins <olegs.ganzins@atea.com>
2022-04-13 22:01:24 +0300

    Adds more example

--- teacher/src/main/java/lv/lu/training/hello/Main.java
+++ teacher/src/main/java/lv/lu/training/hello/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         Person janisKalnins = new Person("Jānis Kalniņš");
 
-        System.out.println(janisKalnins.getName());
+        System.out.println(janisKalnins);
     }
 
 }
