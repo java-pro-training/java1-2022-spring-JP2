e3b02956e2d39d02a81805449e9b1d752d1af222
aburluckis <lawyerlv@icloud.com>
2022-04-13 18:46:15 +0300

    Izveidota SquarePerimeter un TringleAreaCalculator programmas

---
+++ burluckis-andrejs/src/main/java/lv/lu/training/lesson2/SquarePerimeter.java
@@ -0,0 +1,20 @@
+package lv.lu.training.lesson2;
+
+public class SquarePerimeter {
+
+    public static void main(String[] args) {
+
+        int side1 = 10;
+
+        int side2 = 15;
+
+        int side3 = 14;
+
+        int side4 = 25;
+
+        int result = side1 + side2 + side3 +side4;
+
+        System.out.println(result);
+    }
+
+}
