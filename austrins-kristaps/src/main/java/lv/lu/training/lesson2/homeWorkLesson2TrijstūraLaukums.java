4d77f3f9004dc7a24b6e5cac0b717a750441e010
kristapsAustrins <kristaps.austrins@gmail.com>
2022-04-19 20:02:00 +0300

    MājasdarbsLesson3 - Cat

--- austrins-kristaps/src/main/java/lv/lu/training/lesson2/homeWorkLesson2TrijstūraLaukums.java
+++ austrins-kristaps/src/main/java/lv/lu/training/lesson2/homeWorkLesson2TrijstūraLaukums.java
@@ -1,13 +1,13 @@
 package lv.lu.training.lesson2;
 
-public class homeWorkLesson2TrijstūraLaukums {
+ public class homeWorkLesson2TrijstūraLaukums {
 
     public static void main(String[] args) {
 
         int base = 10;
         int height = 20;
 
-        int result = (base * height)/2;
+        int result = (base * height) / 2;
 
         System.out.println("Laukums: " + result);
     }
---
+++ austrins-kristaps/src/main/java/lv/lu/training/lesson3/homeWorkLesson3Cat.java
@@ -0,0 +1,89 @@
+package lv.lu.training.lesson3;
+
+
+    public class homeWorkLesson3Cat {
+
+        private String color;
+        private String name;
+        private String species;
+        private int age;
+        private int weight;
+
+
+    public homeWorkLesson3Cat (String color, String name, int weight) {
+
+        this.color = color;
+        this.name = name;
+        this.weight = weight;
+
+    }
+
+    public String getColor() {
+
+        this.color = color;
+
+    }
+
+    public String getName() {
+
+        this.name = name;
+
+    }
+
+    public String getSpecies() {
+
+        this.species = species;
+
+    }
+
+    public String getAge() {
+
+        this.age = age;
+
+    }
+
+    public String getWeight() {
+
+        this.weight = weight;
+
+    }
+
+    public void setColor(String color) {
+
+        this.color = color;
+
+    }
+
+    public void setName(String name) {
+
+        this.name = name;
+
+    }
+
+    public void setSpecies(String species) {
+
+        this.species = species;
+
+    }
+
+    public void setAge(String age) {
+
+        this.age = age;
+
+    }
+
+    public void setWeight(String weight) {
+
+        this.weight = weight;
+
+    }
+
+}
+
+
+
+
+
+
+
+
