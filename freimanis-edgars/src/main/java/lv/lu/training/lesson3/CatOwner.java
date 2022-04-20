348de40fc7803e654796ee5c0adf944818044e85
Edgars Freimanis <edgars.freimanis@gmail.com>
2022-04-20 18:24:05 +0300

    20.04

--- freimanis-edgars/src/main/java/lv/lu/training/lesson3/CatOwner.java
+++ freimanis-edgars/src/main/java/lv/lu/training/lesson3/CatOwner.java
@@ -3,7 +3,6 @@ package lv.lu.training.lesson3;
 public class CatOwner {
     public static void main (String[] args){
         Cat myCat = new Cat("White and grey", "Putra", 5, 5,"Metiss");
-
         System.out.println("My cat's color - " + myCat.getColor());
         System.out.println("Name - " + myCat.getName());
         System.out.println("Species -" + myCat.getSpecies());
