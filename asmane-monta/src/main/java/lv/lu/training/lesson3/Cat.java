f04328157b6a54584eaac1ee73096bd72a0d2117
montaasmane <monta.asmane@gmail.com>
2022-04-20 18:53:43 +0300

    Izveidota kaķa klase

---
+++ asmane-monta/src/main/java/lv/lu/training/lesson3/Cat.java
@@ -0,0 +1,18 @@
+package lv.lu.training.lesson3;
+
+public class Cat {
+
+    private String color;
+    private String name;
+    private String species;
+    private int age;
+    private int weight;
+
+    public Cat(String color, String name, int weight) {
+        this.color = color;
+        this.name = name;
+        this.weight = weight;
+
+    }
+}
+
