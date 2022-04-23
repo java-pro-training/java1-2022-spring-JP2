d91d7f7a076eb5eda902a46ab687a723d305d3b1
monta <monta.barbina@inbox.lv>
2022-04-23 21:57:08 +0300

    4. lekcija. md.

--- barbina-monta/src/main/java/lv/lu/training/lesson4/RollDice.java
+++ barbina-monta/src/main/java/lv/lu/training/lesson4/RollDice.java
@@ -5,22 +5,20 @@ import java.util.Random;
 public class RollDice {
     public static void main(String[] args) {
 
-
         Random random = new Random();
 
-        int dice1 = random.nextInt(6);
-        dice1 += 1;
-        int dice2 = random.nextInt(6);
-        dice2 += 1;
+        int dice1 = random.nextInt(1,7);
+
+        int dice2 = random.nextInt(1,7);
 
         int result = dice1 + dice2;
 
         if (result == 7 || result == 11)
         {
-            System.out.println("Jūsu izmestais skaitlis ir " + result + ". Apsveicam, Jūs esat spēlē uzvarējis!");
+            System.out.println("Jūsu izmesto skaitļu summa ir " + result + ". Apsveicam, Jūs esat spēlē uzvarējis!");
         }
         else {
-            System.out.println("Jūsu izmestais skaitlis ir " + result + ". Dziemžēl laime Jums šodien nesmaida. Esat zaudējis!");
+            System.out.println("Jūsu izmesto skaitļu summma ir " + result + ". Dziemžēl laime Jums šodien nesmaida. Esat zaudējis!");
         }
 
     }
