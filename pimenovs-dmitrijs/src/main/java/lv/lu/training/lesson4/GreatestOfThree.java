6564e14c7545a7f823ec149767b5fef679930e5a
Dmitrijs <drakowa1991@gmail.com>
2022-04-23 19:24:07 +0300

    GreatestOfThree

---
+++ pimenovs-dmitrijs/src/main/java/lv/lu/training/lesson4/GreatestOfThree.java
@@ -0,0 +1,32 @@
+package lv.lu.training.lesson4;
+
+import java.util.Scanner;
+
+public class GreatestOfThree {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int firstNumber = scanner.nextInt();
+        int secondNumber = scanner.nextInt();
+        int thirdNumber = scanner.nextInt();
+
+        System.out.println("First number" + " " + firstNumber);
+        System.out.println("Second number" + " " + secondNumber);
+        System.out.println("Third number" + " " + thirdNumber);
+
+        if(firstNumber > secondNumber && firstNumber > thirdNumber){
+            System.out.println("First Number is biggest");
+
+        }else if(secondNumber > firstNumber && secondNumber > thirdNumber){
+            System.out.println("Second Number is biggest");
+
+        }else if(thirdNumber > firstNumber && thirdNumber > secondNumber) {
+            System.out.println("Third Number is biggest");
+
+        }else if(firstNumber==secondNumber || firstNumber==thirdNumber){
+            System.out.println("Try to choose another number");
+
+        }else{
+            System.out.println("Try to choose another number");
+        }
+    }
+}
