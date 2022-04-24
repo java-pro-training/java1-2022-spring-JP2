7cde62d5f2b7a7049c16c8d4f2e003c9831724ef
Darja <darja.celigorova@inbox.lv>
2022-04-24 22:52:39 +0300

    4. mājas darbs

--- celigorova-darja/src/main/java/lv/lu/training/lesson3/RandomNumberAdder.java
+++
@@ -1,9 +0,0 @@
-package lv.lu.training.lesson3;
-import java.until.Random;
-public class RandomNumberAdder {
-   public static void main(String[] args) {
-       Random randomNumberGenerator = new Random();
-       int result =
-   }
-
-        }
