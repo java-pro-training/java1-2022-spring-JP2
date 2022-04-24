75482f3be948473f172ce4f9b87815245dfd21f6
aburluckis <lawyerlv@icloud.com>
2022-04-24 11:24:13 +0300

    Izveidota SquarePerimeter un TringleAreaCalculator programmas

---
+++ burluckis-andrejs/src/main/java/lv/lu/training/lesson3/RandomNumberAdder.java
@@ -0,0 +1,22 @@
+package lv.lu.training.lesson3;
+
+import java.util.Random;
+
+public class RandomNumberAdder {
+    public static void main(String[] args) {
+    Random randomNumberGenerator = new Random();
+
+    int result = randomNumberGenerator.nextInt()
+                + randomNumberGenerator.nextInt()
+                + randomNumberGenerator.nextInt();
+
+    System.out.println("Random numbers sum: " + result);
+
+    int firstNumber = randomNumberGenerator.nextInt();
+    int secondNumber = randomNumberGenerator.nextInt();
+    int thirdNumber = randomNumberGenerator.nextInt();
+
+    System.out.println("Unknown number sum: " + (firstNumber + secondNumber + thirdNumber));
+    }
+
+}
