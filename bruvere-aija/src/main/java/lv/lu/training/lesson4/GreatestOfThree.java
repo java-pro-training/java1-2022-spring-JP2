ba765c6d44dc923ee64a6cd49b7f95fb2b78efa9
Aija <aija.bruvere1@gmail.com>
2022-04-27 18:45:07 +0300

    GreatestOfThree.java

--- bruvere-aija/src/main/java/lv/lu/training/lesson4/GreatestOfThree.java
+++ bruvere-aija/src/main/java/lv/lu/training/lesson4/GreatestOfThree.java
@@ -6,22 +6,21 @@ public class GreatestOfThree {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int firstNumber = scanner.nextInt();
         System.out.println("Ievadiet pirmo skaitli:");
+        int firstNumber = scanner.nextInt();
 
-        int secondNumber = scanner.nextInt();
         System.out.println("Ievadiet otro skaitli:");
+        int secondNumber = scanner.nextInt();
 
-        int thirdNumber = scanner.nextInt();
         System.out.println("Ievadiet trešo skaitli:");
+        int thirdNumber = scanner.nextInt();
 
-       if (firstNumber > secondNumber && firstNumber > thirdNumber) {
-           System.out.println("Lielākais skaitlis ir:" + firstNumber);
+        if (firstNumber >= secondNumber && firstNumber >= thirdNumber) {
+            System.out.println("Lielākais skaitlis ir:" + firstNumber);
 
-       } else if (secondNumber > firstNumber && secondNumber > thirdNumber) {
-           System.out.println("Lielākais skaitlis ir:" + secondNumber);
-       } else
-           System.out.println("Lielākais skaitlis ir:" + thirdNumber);
-
+        } else if (secondNumber >= firstNumber && secondNumber >= thirdNumber) {
+            System.out.println("Lielākais skaitlis ir:" + secondNumber);
+        } else
+            System.out.println("Lielākais skaitlis ir:" + thirdNumber);
     }
 }
