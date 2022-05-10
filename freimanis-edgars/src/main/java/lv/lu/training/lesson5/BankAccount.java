8e4fa1aa69439abfac3697b5645b66e77fa2564a
Edgars Freimanis <edgars.freimanis@gmail.com>
2022-05-10 17:03:12 +0300

    10.05

---
+++ freimanis-edgars/src/main/java/lv/lu/training/lesson5/BankAccount.java
@@ -0,0 +1,14 @@
+package lv.lu.training.lesson5;
+
+public class BankAccount {
+        private String accountNumber;
+        private int pinCode;
+        private double debetBalance;
+        private double creditLimit;
+        private double usedCredit;
+
+
+
+
+    }
+
