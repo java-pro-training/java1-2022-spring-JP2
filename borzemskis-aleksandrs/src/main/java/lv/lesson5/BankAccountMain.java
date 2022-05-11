a05ade90ecce6a5660b6527722e90f25b5df9744
alexrulv <alexrulv@gmail.com>
2022-05-11 15:00:56 +0300

    Home Work 5.1 commit

--- borzemskis-aleksandrs/src/main/java/lv/lesson5/BankAccountMain.java
+++ borzemskis-aleksandrs/src/main/java/lv/lesson5/BankAccountMain.java
@@ -4,15 +4,15 @@ public class BankAccountMain {
     public static void main(String[] args) {
         BankAccount janisBerzins = new BankAccount(
                 "LV56HABA254213456413",
-                "4566",
+                "e566",
                 0,
                 200);
         System.out.println(janisBerzins);
-        janisBerzins.topUp(100, "456");
+        janisBerzins.topUp(100, "e566");
         System.out.println(janisBerzins);
-        janisBerzins.withDraw(15,"4566");
+        janisBerzins.withDraw(150,"e566");
         System.out.println(janisBerzins);
-        janisBerzins.topUp(300,"4566");
+        janisBerzins.topUp(300,"E566");
         System.out.println(janisBerzins);
 
     }
