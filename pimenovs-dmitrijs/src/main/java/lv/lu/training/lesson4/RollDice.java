e2e14d19889265289a1716d4609df3a821a9e0fe
Dmitrijs <drakowa1991@gmail.com>
2022-05-15 23:01:17 +0300

    RollDiceAddition

--- pimenovs-dmitrijs/src/main/java/lv/lu/training/lesson4/RollDice.java
+++ pimenovs-dmitrijs/src/main/java/lv/lu/training/lesson4/RollDice.java
@@ -5,23 +5,39 @@ import java.util.Random;
 public class RollDice {
     public static void main(String[] args){
         Random random = new Random();
-        int dice1;
-        int dice2;
+        int sumOfWins = 0;
+        int sumOfLosses = 0;
+int result = 0;
 
-       dice1 = random.nextInt(1,7);
-        dice2 = random.nextInt(1,7);
-        System.out.println("dice1 - " + dice1);
-        System.out.println("dice2 - " + dice2);
+while (result != 12){
 
-        int result = dice1 + dice2;
-        System.out.println("Rezultats" + " " + result);
+    int dice1;
+    int dice2;
+    dice1 = random.nextInt(1,7);
+    dice2 = random.nextInt(1,7);
+    result = dice1 + dice2;
+    System.out.println("dice1 - " + dice1);
+    System.out.println("dice2 - " + dice2);
 
-        if(result==7 || result==11){
-            System.out.println("Spele ir vinnēta");
-
-        }else{
-            System.out.println("Spele ir zaudēta");
-        }
-
+    if (result == 7 || result == 11){
+        sumOfWins++;
+        System.out.println("Result is: " + result);
+        System.out.println("You won");
+        System.out.println("Total games won: " + sumOfWins + " " + "Total games lost: " + sumOfLosses);
+    }else if (result ==2 || result ==3 || result == 6){
+        sumOfLosses++;
+        System.out.println("Result is: " + result);
+        System.out.println("You lose");
+        System.out.println("Total games won: " + sumOfWins + " " + "Total games lost: " + sumOfLosses);
+    }else if (result == 12){
+        System.out.println("Result is: " + result);
+        System.out.println("Game finished");
+        System.out.println("Total games won: " + sumOfWins + " " + "Total games lost: " + sumOfLosses);
+    }else{
+        System.out.println("Result is: " + result);
+        System.out.println("Try again");
+        System.out.println("Total games won: " + sumOfWins + " " + "Total games lost: " + sumOfLosses);
+    }
+}
     }
 }
