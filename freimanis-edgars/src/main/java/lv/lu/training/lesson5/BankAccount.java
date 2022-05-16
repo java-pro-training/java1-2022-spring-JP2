8c8b17cc75a3551ebb29e6e882c475e0b1811788
Edgars Freimanis <edgars.freimanis@gmail.com>
2022-05-16 17:18:14 +0300

    16.05

--- freimanis-edgars/src/main/java/lv/lu/training/lesson5/BankAccount.java
+++ freimanis-edgars/src/main/java/lv/lu/training/lesson5/BankAccount.java
@@ -2,11 +2,24 @@ package lv.lu.training.lesson5;
 
 public class BankAccount {
         private String accountNumber;
-        private int pinCode;
-        private double debetBalance;
+        private String pinCode;
+        private double balance;
         private double creditLimit;
         private double usedCredit;
 
+        public void enterPinCode(String pinCode){
+                this.pinCode = pinCode;
+        }
+        public void topUp (double amount){
+                balance += amount;
+        }
+        public void withdraw(double amount){
+                balance -=amount;
+        }
+        private boolean isPinCodeValid(){
+                return pinCode !=null
+                        && pinCode.length()==4;
+        }
 
 
 
