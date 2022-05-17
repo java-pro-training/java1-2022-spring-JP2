35cd1356c3ef04fd096f873c1d0fcc781762f581
Dmitrijs <drakowa1991@gmail.com>
2022-05-17 22:27:24 +0300

    BankAcc+BankAccUser v2

--- pimenovs-dmitrijs/src/main/java/lv/lu/training/lesson5/BankAccUser.java
+++ pimenovs-dmitrijs/src/main/java/lv/lu/training/lesson5/BankAccUser.java
@@ -5,22 +5,53 @@ import java.util.Scanner;
 public class BankAccUser {
 
         public static void main(String[] args) {
-
-            Scanner scanner = new Scanner(System.in);
+Scanner amountScanner = new Scanner(System.in);
+Scanner opScanner = new Scanner(System.in);
+            Scanner pinScanner = new Scanner(System.in);
             BankAccount bankAccount = new BankAccount("LV67PARX1133144", "2415", 100, 200, 0);
+            char operation;
+            int tries = 0;
 
-            bankAccount.enterPinCode(scanner.next());
-            bankAccount.withdraw(150);
-            System.out.println("bank acc status" + bankAccount);
+            while (tries < 3){
+                tries++;
+                System.out.println("Enter Pin Code: ");
+                String enteredPin = pinScanner.next();
+              if (enteredPin.equals(bankAccount.getPinCode())){
+                  System.out.println("Welcome");
+                  bankAccount.enterPinCode(bankAccount.getPinCode());
+                  tries=0;
 
-            bankAccount.enterPinCode(scanner.next());
-            bankAccount.topUp(20);
-            System.out.println("bank acc status: " + bankAccount);
-
-            bankAccount.enterPinCode(scanner.next());
-            bankAccount.withdraw(205);
-            System.out.println("bank acc status" + bankAccount);
-
+                  System.out.println("Choose operation: t - topUp; w - withdraw; b - balance; e - exit");
+                  operation = opScanner.next().charAt(0);
+                  switch (operation){
+                      case 't':
+                          System.out.println("TopUp amount: ");
+                          bankAccount.topUp(amountScanner.nextInt());
+                          System.out.println("Account balance: " +bankAccount);
+                          continue;
+                      case 'w':
+                          System.out.println("Withdraw amount: ");
+                          bankAccount.withdraw(amountScanner.nextInt());
+                          System.out.println("Account balance: " + bankAccount);
+                          continue;
+                      case 'b':
+                          System.out.println("Account balance: " + bankAccount);
+                          continue;
+                      case 'e':
+                          System.out.println("Thanks for using our service!!");
+                          break;
+                      default:
+                          System.out.println("Incorrect operation");
+                          continue;
+                  }
+                  break;
+                }else{
+                  System.out.println("Incorrect Pin");
+                }
+                if (tries==3){
+                    System.out.println("Account Blocked");
+                }
+            }
         }
     }
 
