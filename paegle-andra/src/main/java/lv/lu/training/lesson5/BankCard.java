c5044705ce88709fa3d504d9eb75205e5308e83a
andra <andra.paegle@gmail.com>
2022-05-21 18:51:48 +0300

    Izveidota programma RollDice

---
+++ paegle-andra/src/main/java/lv/lu/training/lesson5/BankCard.java
@@ -0,0 +1,52 @@
+package lv.lu.training.lesson5;
+
+import java.time.LocalDate;
+
+    public class BankCard {
+
+        private String bankName;
+        private String origin;
+        private String holderName;
+        private String number;
+        private LocalDate expiryDate;
+        private String cvv;
+        private double balance;
+
+        public BankCard(String bankName, String origin, String holderName, String number,
+                        LocalDate expiryDate, String cvv, double balance) {
+            this.bankName = bankName;
+            this.origin = origin;
+            this.holderName = holderName;
+            this.number = number;
+            this.expiryDate = expiryDate;
+            this.cvv = cvv;
+            this.balance = 0;
+        }
+
+        public BankCard(String swedbank, String master, String andra_paegle, String number, LocalDate of, String cvv) {
+        }
+
+        public void withdraw(double value) {
+            this.balance -= value;
+        }
+
+        public void addFunds(double value) {
+            this.balance += value;
+        }
+
+        @Override
+        public String toString() {
+            return "BankCard{" + "bankName='" + bankName + "' " +
+                    ", origin='" + origin + "' " +
+                    ", holderName='" + holderName + "' " +
+                    ", number='" + number + "' " +
+                    ", expiryDate=" + expiryDate +
+                    ", cvv='" + cvv + "' " +
+                    ", balance=" + balance +
+                    '}';
+
+
+        }
+    }
+
+
