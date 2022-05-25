1c2dd264eb9815d818334675dfe57acbf92302f4
andra <andra.paegle@gmail.com>
2022-05-25 18:01:03 +0300

    Izveidota programma RollDice

--- paegle-andra/src/main/java/lv/lu/training/lesson4/RollDice.java
+++ paegle-andra/src/main/java/lv/lu/training/lesson4/RollDice.java
@@ -5,8 +5,8 @@ import java.util.Random;
 public class RollDice {
     public static void main(String[] args) {
         Random random = new Random();
-        int dice1 = random.nextInt(1,7);
-        int dice2 = random.nextInt(1,7);
+        int dice1 = random.nextInt(1,6);
+        int dice2 = random.nextInt(1,6);
         int result = dice1 + dice2;
         System.out.println(result);
         if ((result != 7) && (result != 11)) {
