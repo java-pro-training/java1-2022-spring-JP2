6b7e86a31d209c2b87f57176abd38e9675f5e10a
Dmitrijs <drakowa1991@gmail.com>
2022-05-25 19:22:25 +0300

    palindrome

--- pimenovs-dmitrijs/src/main/java/lv/lu/training/lesson7/ArrayUtilsTest.java
+++ pimenovs-dmitrijs/src/main/java/lv/lu/training/lesson7/ArrayUtilsTest.java
@@ -1,13 +1,17 @@
 package lv.lu.training.lesson7;
 
+import java.util.Arrays;
+
 public class ArrayUtilsTest {
     public static void main(String[] args) {
         int[] result = ArrayUtils.reverse(new int[]{1,2,3});
+        int[] something = ArrayUtils.reverse(result);
         System.out.println("Testejam klases ArrayUtils metodi 'reverse',darbojas..");
         if (result[0]==3 && result[1]==2 && result[2]==1){
             System.out.println("PAREIZI");
         }else{
             System.out.println("NEPAREIZI");
         }
+        System.out.println(Arrays.toString(something));
     }
 }
