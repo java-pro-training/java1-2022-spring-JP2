1bbc7917e3e5ee2f0bb6fb48618545e58a750db4
DaigaSmeke <daigasmeke@gmail.com>
2022-05-29 14:55:19 +0300

    8.1. Car+Truck+Bus+Passenger+Parking

---
+++ smeke-daiga/src/main/java/lv/lu/training/lesson8/Buss.java
@@ -0,0 +1,38 @@
+package lv.lu.training.lesson8;
+
+public class Buss extends Car implements Passenger {
+    private final int passengerSeatCount;
+
+    public Buss(String color, int year, String manufacturer, int passengerSeatCount) {
+        super(color, year, manufacturer);
+        this.passengerSeatCount = passengerSeatCount;
+    }
+
+    @Override
+    public int passengerSeatCount() {
+        return this.passengerSeatCount;
+    }
+
+    @Override
+    public String toString() {
+        return "Buss{" +
+                "color='" + getColor() + '\'' +
+                ", year='" + getYear() + '\'' +
+                ", manufacturer='" + getManufacturer() + '\'' +
+                ", passengerSeatCount=" + passengerSeatCount +
+                "} ";
+    }
+
+    @Override
+    public boolean equals(Object that) {
+        if (this == that) return true;
+        if (that == null || getClass() != that.getClass()) return false;
+        Buss buss = (Buss) that;
+        return passengerSeatCount == buss.passengerSeatCount
+                && getColor().equals(buss.getColor())
+                && getYear() == buss.getYear()
+                && getManufacturer().equals(buss.getManufacturer());
+    }
+
+
+}
