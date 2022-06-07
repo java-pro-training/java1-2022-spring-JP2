04550677376973fc8b88e53c463e454098a96a74
montaasmane <monta.asmane@gmail.com>
2022-06-07 18:36:31 +0300

    7.lekcijas mājas darbs

--- asmane-monta/src/main/java/lv/lu/training/lesson5/BankAccount.java
+++ asmane-monta/src/main/java/lv/lu/training/lesson5/BankAccount.java
@@ -1,9 +1,46 @@
 package lv.lu.training.lesson5;
 
 public class BankAccount {
-    private int bankAccount;
-    private int pinCode;
-    private double debitBalance;
-    private double creditBalance;
+    private String accountNumber;
+    private String pinCode;
+    private double balance;
+    private double creditLimit;
+    private double usedCredit;
+    private boolean isPinCodeValid;
 
+    public BankAccount(String accountNumber, String pinCode, double balance,
+                       double creditLimit) {
+        this.accountNumber = accountNumber;
+        this.pinCode = pinCode;
+        this.balance = balance;
+        this.creditLimit = creditLimit;
+        this.usedCredit = 0.0;
+    }
+
+    public void enterPinCode(String pinCode) {
+        isPinCodeValid = this.pinCode.equals(pinCode);
+    }
+
+        public void topUp(double amount) {
+        if (isPinCodeValid) {
+            balance += amount;
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (isPinCodeValid) {
+            if (balance - amount >= 0) {
+                balance -= amount;
+            } else if ((balance + creditLimit - usedCredit) - amount >= 0)
+                balance -= amount;
+                usedCredit -= balance;
+                balance = 0;
+            }
+        }
+
+    public void getBalance() {
+
+    }
 }
+
+
