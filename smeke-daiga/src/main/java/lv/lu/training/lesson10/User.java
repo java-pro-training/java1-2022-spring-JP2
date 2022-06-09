db62210e62ee2aab46059af6252938765a6f6aac
DaigaSmeke <daigasmeke@gmail.com>
2022-06-09 12:51:51 +0300

    homework 10.1

---
+++ smeke-daiga/src/main/java/lv/lu/training/lesson10/User.java
@@ -0,0 +1,29 @@
+package lv.lu.training.lesson10;
+
+//Izveidojam jaunu klasi User ar nepieciešamajiem laukiem un konstruktoru un geteriem
+
+public class User {
+
+    private final String name;
+    private final String surname;
+    private final int age;
+
+    public User(String name, String surname, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public int getAge() {
+        return age;
+
+    }
+}
