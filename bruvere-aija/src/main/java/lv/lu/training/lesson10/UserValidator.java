75e98a568a187fdd34dcb54b01dc74046c4c9dcb
Aija <aija.bruvere1@gmail.com>
2022-06-13 21:38:57 +0300

    Desmitās lekcijas 1.mājasdarbs

---
+++ bruvere-aija/src/main/java/lv/lu/training/lesson10/UserValidator.java
@@ -0,0 +1,34 @@
+package lv.lu.training.lesson10;
+
+public class UserValidator {
+
+        private static final int MIN_TEXT_LENGTH = 3;
+        private static final int MAX_TEXT_LENGTH = 15;
+        private static final int MIN_AGE = 0;
+        private static final int MAX_AGE = 120;
+
+    public void validate(User user) {
+        if (hasInvalidLength(user.getName()) || hasInvalidLength(user.getSurname())) {
+            throw new ValidationException("Name and surname has to be 3 to 15 characters long");
+        }
+    }
+    private boolean hasInvalidLength(String text) {
+        return text.length() < MIN_TEXT_LENGTH || text.length() > MAX_TEXT_LENGTH;
+    }
+    private boolean ageCheck(int age) {
+        return age < MIN_AGE || age > MAX_AGE;
+    }
+
+    private void validate(String name, String surname, int age) {
+        if (ageCheck(age)){
+            throw  new ValidationException("Age must be within human possibility : 0 - 120");
+        }
+    }
+
+    private boolean hasInvalidAge(int age) {
+        return age < MIN_AGE || age > MAX_AGE;
+    }
+
+}
+
+
