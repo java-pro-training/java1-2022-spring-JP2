001dcdebb1fdf4c422aa0053ad0560d2c7bd618e
Olegs Ganzins <olegs.ganzins@atea.com>
2022-06-15 21:58:14 +0300

    Lesson 10

---
+++ teacher/src/main/java/lv/lu/training/lesson10/homework2/ValidationException.java
@@ -0,0 +1,9 @@
+package lv.lu.training.lesson10.homework2;
+
+public class ValidationException extends RuntimeException {
+
+    public ValidationException(String message) {
+        super(message);
+    }
+
+}
