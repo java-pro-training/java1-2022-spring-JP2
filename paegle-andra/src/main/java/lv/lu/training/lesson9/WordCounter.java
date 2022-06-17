910575cff435d252e7ade6e42148d4d5f9a75e26
andra <andra.paegle@gmail.com>
2022-06-17 16:31:29 +0300

    Izveidota programma WordCounter

---
+++ paegle-andra/src/main/java/lv/lu/training/lesson9/WordCounter.java
@@ -0,0 +1,42 @@
+package lv.lu.training.lesson9;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class WordCounter {
+
+    public static void main(String[] args) {
+        Map<String, Integer> wordCounts = new HashMap<>();
+
+        Scanner input = new Scanner(System.in);
+        while (true) {
+            System.out.println("Lūdzu ievadiet tekstu vai quit: ");
+            String line = input.nextLine();
+            if (line.contains("quit")) break;
+            for (String word : line.split(" ")) {
+                addWord(word, wordCounts);
+            }
+
+            for (String word : wordCounts.keySet()) {
+                System.out.println(word + " = " + wordCounts.get(word));
+            }
+            input.close();
+        }
+    }
+
+    private static void addWord(String word, Map<String, Integer> wordCounts) {
+        wordCounts.put(word,incrementCounter(word, wordCounts));
+    }
+
+    private static Integer incrementCounter(String word, Map<String, Integer> wordCounts) {
+        Integer wordCount = wordCounts.get(word);
+        if (wordCount == null) {
+            return 1;
+        }
+        return ++wordCount;
+    }
+}
+
+
+
