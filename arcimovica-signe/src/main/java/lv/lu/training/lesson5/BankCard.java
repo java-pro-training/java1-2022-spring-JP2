e5e5cceabb593b543283b496747005970b35e145
signe <signe.arcimovica@gmail.com>
2022-06-18 13:33:14 +0300

    Izveidoti 7.lekcijas mājasdarbi.

--- arcimovica-signe/src/main/java/lv/lu/training/lesson5/BankCard.java
+++ arcimovica-signe/src/main/java/lv/lu/training/lesson5/BankCard.java
@@ -49,3 +49,4 @@ public class BankCard { //kartei izveido privātus datus
                 '}';
     }
 }
+
---
+++ arcimovica-signe/src/main/java/lv/lu/training/lesson7/Palindrome.java
@@ -0,0 +1,20 @@
+package lv.lu.training.lesson7;
+
+public class Palindrome {
+
+    public static boolean isPalindrome(String text) {
+        var chars=text.toLowerCase().toCharArray();
+        for(int i=0, j=chars.length-1; i< chars.length/2; i++, j--){
+            if (chars[i] !=chars[j]){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("Racecar"));
+        System.out.println(isPalindrome("Level"));
+        System.out.println(isPalindrome("Sum summus mus"));
+    }
+   }
