942b15ba217fd7b94067eb4aaf7a00d543e7df74
signe <signe.arcimovica@gmail.com>
2022-06-19 18:36:30 +0300

    Izveidoti 9.lekcijas mājasdarbi.

---
+++ arcimovica-signe/src/main/java/lv/lu/training/lesson9/AlternateWordCount.java
@@ -0,0 +1,40 @@
+package lv.lu.training.lesson9;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class AlternateWordCount {
+    private Map<String,Integer> wordCounts = new HashMap<>();
+
+    public static void main(String[] args) {
+        AlternateWordCount wordCount = new AlternateWordCount();
+        wordCount.run();
+    }
+    void run(){
+
+        String[] wordArray = new String[]
+                {"Zoologija",
+                        "Matematika",
+                        "Zoologija",
+                        "Literatura",
+                        "Matematika"};
+
+        List<String> wordList= Arrays.asList(wordArray);
+
+        for(String word:wordList){
+            addWord(word,wordCounts);
+        }
+        System.out.println(wordCounts);
+    }
+    public void addWord(String word, Map<String, Integer> wordCounts) {
+        Integer wordCount = wordCounts.get(word);
+        if (wordCount == null){
+            wordCount = 1;
+        }else {
+            wordCount++; //wordCount = wordCount + 1;
+        }
+        wordCounts.put(word, wordCount);
+    }
+}
