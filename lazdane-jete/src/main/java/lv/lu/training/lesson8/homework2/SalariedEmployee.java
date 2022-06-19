01422c4a3d5ebce2ef2ef8edd94dab94b3745893
jlazdane <jlazdane@gmail.com>
2022-06-19 19:16:43 +0300

    Astotās lecijas otrais mājas darbs

---
+++ lazdane-jete/src/main/java/lv/lu/training/lesson8/homework2/SalariedEmployee.java
@@ -0,0 +1,29 @@
+package lv.lu.training.lesson8.homework2;
+
+public class SalariedEmployee extends Employee {
+    private double weeklySalary;
+
+    public SalariedEmployee(String firstName, String lastName, String socialSecurityNumber, double weeklySalary) {
+        super(firstName, lastName, socialSecurityNumber);
+        this.weeklySalary = weeklySalary;
+    }
+
+    public void setWeeklySalary(double weeklySalary) {
+        this.weeklySalary = weeklySalary;
+    }
+
+    public double getWeeklySalary() {
+        return weeklySalary;
+    }
+
+    @Override
+    public double pay() {
+        return weeklySalary;
+    }
+
+    @Override
+    public String toString() {
+        return "Salaried employee: Employee " + getFirstName() + " " + getLastName() + "  " + getSocialSecurityNumber() + " weekly salary: " + getWeeklySalary();
+    }
+}
+
